4e029a2da8a5f74aa1cc3e485abb00be5b1614ef
Averndenal <amaury.y.verdenal@gmail.com>
2020-02-07 15:47:57 +0100

    add commentaire

--- controllers/ControllerComment.php
+++ controllers/ControllerComment.php
@@ -3,13 +3,17 @@
 class ControllerComment
 {
     private $_commentManager;
+    private $_articleManager;
     function __construct()
     {
         $this->_commentManager = new CommentManager();
+        $this->_articleManager = new ArticleManager();
     }
 
     public function addComment($info)
     {
         $this->_commentManager->addComment($info['content'],$info['article']);
+        $article = $this->_articleManager->getArticleById($info['article']);
+        header("location:".ROOT."/Article/".$article->getSlug().'-'.$article->getId(),201);
     }
 }
