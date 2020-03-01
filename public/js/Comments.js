91215ad569cc1bc1f1c960e59eb4d54e0b882647
averdenal <amaury.y.verdenal@gmail.com>
2020-03-01 19:48:52 +0100

    comment

--- public/js/Comments.js
+++ public/js/Comments.js
@@ -5,6 +5,7 @@ class Comments
     {
         this.delete_com();
         this.add_Comment();
+        this.basepath = "/P4_Code_2";
     }
 
     delete_com()
@@ -52,7 +53,9 @@ class Comments
                         textarea.value = "";
                         infomsg.innerHTML = 'Commentaire Ajouté '
                         let comments = JSON.parse(HttpRequest.responseText);
-                        console.log(comments);
+                        comments.forEach(element => {
+                            create_Comment(element.comment,element.autorIsConnect,element,warningByConnnect);
+                        });
                     }
                 }
                 let data = new FormData(formv)
@@ -63,8 +66,42 @@ class Comments
             });
         }
     }
-    create_Comment(){
+    create_Comment(comment,autor,warning){
+        let item_Comment = document.createElement('div');
+        item_Comment.item_Comment.classList.add("item_Comment");
 
+            let comment_Content = document.createElement('p');
+            comment_Content.innerHTML = comment.content;
+        item_Comment.appendChild(comment_Content);
+
+        let comment_Info = document.createElement('div');
+            let comment_Date = document.createElement('p');
+            comment_Date.innerHTML = comment.date;
+            comment_Info.appendChild(comment_Date);
+
+            let comment_Autor = document.createElement('p');
+            comment_Autor.innerHTML = comment.firstname+' '+comment.lastname;
+            comment_Info.appendChild(comment_Autor);
+        item_Comment.appendChild(comment_Info);
+
+            let action = document.createElement('div');
+            if(autor == 1){
+                let action_A_Delete = document.createElement('a');
+                action_A_Delete.classList.add('btn');
+                action_A_Delete.classList.add('btn_Delete');
+                action_A_Delete.href = this.basepath+'/Articles/deleteComment/'+comment.id+'/'+comment.article;
+                action.appendChild(action_A_Delete);
+            }
+            if(warning != 1){
+                let action_A_Warning = document.createElement('a');
+                action_A_Warning.classList.add('btn');
+                action_A_Warning.classList.add('btn_Warning');
+                action_A_Warning.href = this.basepath+'/Articles/addWarning/'+comment.id+'/'+comment.article;
+                action.appendChild(action_A_Warning);
+            }
+                
+        item_Comment.appendChild(action);
+        return item_Comment;
     }
     
 }
