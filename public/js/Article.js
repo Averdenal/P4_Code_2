9ecd061da356f6f6980205d4d63ea6ebff4c755b
averdenal <amaury.y.verdenal@gmail.com>
2020-03-08 00:51:55 +0100

    ajax article

--- public/js/Article.js
+++ public/js/Article.js
@@ -8,7 +8,7 @@ class Article
     }
     newArticle(){
         var formAddArticle = document.getElementById('add_Article');
-        var info = document.getElementById('container_Article');
+        //var info = document.getElementById('container_Article');
         var msg = document.getElementById('info');
         if(formAddArticle !== null){
             formAddArticle.addEventListener('submit',function(e){
@@ -24,7 +24,6 @@ class Article
                 data.append('content',tinyMCE.get('Form_content').getContent());
                 HttpRequest.open('POST','/P4_Code_2/Administration/createArticle',true);
                 HttpRequest.send(data);
-                
             });
         }
     }
@@ -34,22 +33,19 @@ class Article
         document.body.addEventListener('click', (evt) => {
             if (evt.target.className === 'btn btn_Delete_Article') {
                 evt.preventDefault();
-                let HttpRequest = new XMLHttpRequest();
                 let infomsg  = document.querySelector('#info');
                 let infoArticle = document.querySelector('#list_Articles')
-                HttpRequest.onreadystatechange = () => {
-                    if(HttpRequest.readyState === 4){
-                        infomsg.innerHTML = 'Article Supprimé';
-                        infoArticle.innerHTML =" ";
-                        let articles = JSON.parse(HttpRequest.responseText);
-                        articles.forEach(article => {
-                            infoArticle.appendChild(this.createListArticle(article));
-                        });
-
-                    }
-                }
-                HttpRequest.open('DELETE',evt.target.pathname,true);
-                HttpRequest.send();
+                $.ajax({
+                    type: "DELETE",
+                    url: evt.target.pathname,
+                }).done((reponse) => {
+                    infomsg.innerHTML = 'Article Supprimé';
+                    infoArticle.innerHTML =" ";
+                    let articles = JSON.parse(reponse);
+                    articles.forEach(article => {
+                        infoArticle.appendChild(this.createListArticle(article));
+                    });
+                });
             }
         });
     }
@@ -78,45 +74,15 @@ class Article
     }
 
     createListArticle(article){
-        console.log(article);
-        let elementHtml_tr = document.createElement('tr');
-
-            let elementHtml_Td_Id = document.createElement('td');
-            elementHtml_Td_Id.innerHTML = article.id;
-            elementHtml_tr.appendChild(elementHtml_Td_Id);
-
-            let elementHtml_Td_Date = document.createElement('td');
-            elementHtml_Td_Date.innerHTML = article.date;
-            elementHtml_tr.appendChild(elementHtml_Td_Date);
-
-            let elementHtml_Td_Title = document.createElement('td');
-            elementHtml_Td_Title.innerHTML = article.title;
-            elementHtml_tr.appendChild(elementHtml_Td_Title);
-
-            let elementHtml_Td_Content = document.createElement('td');
-            elementHtml_Td_Content.innerHTML = article.content;
-            elementHtml_tr.appendChild(elementHtml_Td_Content);
-
-            let elementHtml_Td_Autor = document.createElement('td');
-            elementHtml_Td_Autor.innerHTML = article.lastname+' '+article.firstname;
-            elementHtml_tr.appendChild(elementHtml_Td_Autor);
-
-            let elementHtml_Td_Action = document.createElement('td');
-            let elementHtml_A_Edit = document.createElement('a');
-            elementHtml_A_Edit.classList.add("btn");
-            elementHtml_A_Edit.classList.add("btn_Edit_Article");
-            elementHtml_A_Edit.href = this.basepath+"/Administration/editArticle/"+article.id;
-            elementHtml_A_Edit.innerHTML = "Editer"
-            elementHtml_Td_Action.appendChild(elementHtml_A_Edit);
-
-            let elementHtml_A_Delete = document.createElement('a');
-            elementHtml_A_Delete.classList.add("btn");
-            elementHtml_A_Delete.classList.add("btn_Delete_Article");
-            elementHtml_A_Delete.href = this.basepath+"/Administration/deleteArticle/"+article.id;
-            elementHtml_A_Delete.innerHTML = "Supprimer";
-            elementHtml_Td_Action.appendChild(elementHtml_A_Delete);
-
-            elementHtml_tr.appendChild(elementHtml_Td_Action);
-        return elementHtml_tr;    
+        var articleHtml = $("<tr>"+
+        "<td>"+article.id+"</td>"+
+        "<td>"+article.date+"</td>"+
+        "<td>"+article.title+"</td>"+
+        "<td>"+article.content+"</td>"+
+        "<td>"+article.lastname+" "+article.firstname+"</td>"+
+        "<td><a class='btn btn_Edit_Article' href='"+this.basepath+"/Administration/editArticle/"+article.id+"'>Edit</a>"+
+        "<a class='btn btn_Delete_Article' href='"+this.basepath+"/Administration/deleteArticle/"+article.id+"'>Supprimer</a></td>"+
+        "</tr>")
+        return articleHtml[0];    
     }
 }
