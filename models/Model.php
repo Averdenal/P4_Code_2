443313739907c49fe9577e731aafe179c667b245
averdenal <amaury.y.verdenal@gmail.com>
2020-03-27 03:49:59 +0100

    coorection

--- models/Model.php
+++ models/Model.php
@@ -4,9 +4,6 @@
 abstract class Model{
     private static $bdd;
 
-    private static function setBdd1(){           
-        self::$bdd = new PDO('mysql:host=localhost;dbname=mydb;charset=utf8', 'root', 'root');
-    }
     private static function setBdd(){
         self::$bdd = new PDO('mysql:host=localhost;dbname=elipqfye_p4openclassrooms;charset=utf8', 'elipqfye_p4-certif', 'U8l}0=s!!de;$GNKV2');
     }
